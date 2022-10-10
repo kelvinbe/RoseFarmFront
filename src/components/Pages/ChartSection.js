5b9bf7d2820adfe92236cee30dd9e7ad88305a80
kelvin beno <kelvinbeno526@gmail.com>
2022-10-10 14:24:56 +0300

    added video section

--- src/components/Pages/ChartSection.js
+++ src/components/Pages/ChartSection.js
@@ -1,31 +1,62 @@
-import React, {useEffect, useRef, useState} from "react";
+import React from "react";
 import Grid from "@mui/material/Grid";
+import Spidy from "../../Assets/eyes.jpg";
+import Sink from "../../Assets/infernal.png";
+import Face from "../../Assets/kratos.jpg";
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 import Box from "@mui/material/Box";
-import Header from "../Header/Header";
 
-import "./End.css";
+import "./Show.css";
 import { Typography } from "@mui/material";
-import RatingsChart from "../Charts/RatingsChart";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
+import frame1 from '../../Assets/frame_1.png'
+import frame2 from '../../Assets/frame_2.png'
+import frame3 from '../../Assets/frame_3.png'
+import shamba from '../../components/Videos/shambaa.mp4'
+
 AOS.init()
+console.log('ooo')
 
-export default function ChartSection() {
-  const myRef = useRef()
-  const [chartIsVisible, setChartIsVisible] = useState()
-  console.log('chartVisible', chartIsVisible)
-  useEffect(() => { 
+const slideImages = [
+  {
+    url: Sink,
+  },
+  {
+    url: Spidy,
+  },
+  {
+    url: Face,
+  },
+];
 
-    const observer = new IntersectionObserver((entries) => {
-      const entry= entries[0]
-      setChartIsVisible(entry.isIntersecting)
+const Slideshow = () => {
+  return (
+    <div className="slide-container">
+      <Slide>
+        {slideImages.map((slideImage, index) => (
+          <div className="each-slide" key={index}>
+            <div
+              style={{
+                backgroundImage: `url(${slideImage.url})`,
+                height: 480,
+                borderRadius: 20,
+              }}
+            >
+              <span>{slideImage.caption}</span>
+            </div>
+          </div>
+        ))}
+      </Slide>
+    </div>
+  );
+};
 
-  }, [])
-  observer.observe(myRef.current)
-
-  })
-  
+export default function Show() {
   return (
     <Box
       sx={{
@@ -36,14 +67,38 @@ export default function ChartSection() {
         alignItems: "center",
       }}
     >
-      <Header />
-      <Grid container className="container-landing">
-        <Typography className="text-end">
-          <h1>Number of games on various platforms</h1>
-          <h4 >Just how many are games are there on the various gaming platforms check out the graph below</h4>
-        </Typography>
-        <Grid ref={myRef}  item xs={12} className="graphh">
-          <RatingsChart chartIsVisible={chartIsVisible} />
+      <Grid container className="container-show">
+      
+        <Grid item>
+          <Card src={shamba} >
+
+            <CardMedia
+             component="video"
+             height="300"
+             image=""
+             src={shamba}
+             autoPlay
+             muted
+             />
+          </Card>
+        </Grid>
+        <Grid item className="img-game">
+          <Typography style={{fontFamily: 'Libre Baskerville', color: '#023F3A'}}>
+            <h1 >Everyone should have access to nutritious food
+      </h1>
+      <h4>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+      Aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </h4>
+          </Typography>
+          <Grid item>
+          <img src={frame1} />
+          <p>Organic making</p>
+        
+        </Grid>
         </Grid>
       </Grid>
     </Box>
--- src/components/Pages/Show.js
+++ src/components/Pages/Show.js
@@ -12,6 +12,12 @@ import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
+import farm from '../../Assets/Farm.png'
+import farm1 from '../../Assets/Farm_1.png'
+import farm2 from '../../Assets/Farm_2.png'
+import frame1 from '../../Assets/frame_1.png'
+import frame2 from '../../Assets/frame_2.png'
+import frame3 from '../../Assets/frame_3.png'
 
 AOS.init()
 console.log('ooo')
@@ -55,7 +61,7 @@ export default function Show() {
     <Box
       sx={{
         flexGrow: 1,
-        backgroundColor: "black",
+        backgroundColor: "white",
         height: "100vh",
         justifyContent: "center",
         alignItems: "center",
@@ -64,7 +70,7 @@ export default function Show() {
       <Grid container className="container-show">
         <Grid item className="img-game">
           <Typography className="text-show">
-            <h1 >Next Generation Games</h1>
+            <h1 >Mission we are working on</h1>
             <h4 >See what next Gen Games look like from incredible detailed graphics,
               To the surreal power of the consoles imporving not only loading time,
               but player experience overall.
@@ -72,8 +78,31 @@ export default function Show() {
               
                </h4>
           </Typography>
+          <Grid item>
+          <img src={frame1} />
+          <p>Organic making</p>
+          <Grid item style={{display: 'flex', justifyContent: 'center'}}>
+            <div>
+            <img style={{margin: 30}} src={frame2} />
+          <p>Certified Products</p>
+          </div>
+            <div>
+            <img style={{margin: 30}} src={frame3} />
+          <p style={{marginLeft: 10}}>Fast Delivery</p>
+          </div>
+
+          </Grid>
         </Grid>
-        <Grid item>{Slideshow()}</Grid>
+        </Grid>
+        <Grid item>
+          <img src={farm} />
+          <Grid item>
+            <img style={{margin: 20}} src={farm1} />
+            <img style={{margin: 20}} src={farm2} />
+
+          </Grid>
+        
+        </Grid>
       </Grid>
     </Box>
   );
